7910f5334389a173df127f8c8a13027a1714c4fe
MariaAmr <mariamamr528@gmail.com>
2025-08-09 23:28:43 +0300

    update for vercel2

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,8 +1,16 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
+  { path: "home", renderMode: RenderMode.Server },
+  { path: "about", renderMode: RenderMode.Server },
+  { path: "meals", renderMode: RenderMode.Server },
+  { path: "meals/seafood", renderMode: RenderMode.Server },
+  { path: "meals/pasta", renderMode: RenderMode.Server },
+  { path: "meals/pizza", renderMode: RenderMode.Server },
+  { path: "team", renderMode: RenderMode.Server },
+  { path: "contacts", renderMode: RenderMode.Server },
   {
-    path: '**',
-    renderMode: RenderMode.Prerender
-  }
+    path: "**",
+    renderMode: RenderMode.Client,
+  },
 ];
