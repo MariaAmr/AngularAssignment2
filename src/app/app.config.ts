54e5e7148dfa5adfef8fc87475eeff3065ffd791
MariaAmr <mariamamr528@gmail.com>
2025-08-10 00:15:10 +0300

    update for vercel3

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -11,7 +11,7 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(), 
     provideZonelessChangeDetection(),
-    provideRouter(routes),
+    provideRouter(routes,withHashLocation()),
     provideClientHydration(withEventReplay()),
     provideHttpClient(),
     provideLottieOptions({
--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,18 +1,8 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
-  { path: "", renderMode: RenderMode.Server },
-  { path: "home", renderMode: RenderMode.Server },
-  { path: "about", renderMode: RenderMode.Server },
-  { path: "meals", renderMode: RenderMode.Server },
-  { path: "meals/seafood", renderMode: RenderMode.Server },
-  { path: "meals/pasta", renderMode: RenderMode.Server },
-  { path: "meals/pizza", renderMode: RenderMode.Server },
-  { path: "team", renderMode: RenderMode.Server },
-  { path: "contacts", renderMode: RenderMode.Server },
-  { path: "not-found", renderMode: RenderMode.Server },
   {
-    path: "**",
-    renderMode: RenderMode.Server,
-  },
+    path: '**',
+  renderMode: RenderMode.Client
+  }
 ];
